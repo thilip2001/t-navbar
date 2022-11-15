216f0d34688c9fca88a56fb71c2b115636056b6a
Thiliban <thilip2017@gmail.com>
2022-11-15 15:47:05 +0530

    Navbar updated

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,19 +1,39 @@
 import React, { useState, useRef, useEffect } from "react";
-import { FaBars, FaTwitter } from "react-icons/fa";
+import { FaBars} from "react-icons/fa";
 import { links, social } from "./data";
 
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+  const toggleLinks = () => {
+    setShowLinks(!showLinks);
+  };
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = "0px";
+    }
+  }, [showLinks]);
+
   return (
     <nav>
       <div className="nav-center">
         <div className="nav-header">
           <h3>T Portfolio</h3>
-          <button className="nav-toggle">
+          <button
+            className="nav-toggle"
+            onClick={toggleLinks}
+          >
             <FaBars />
           </button>
         </div>
-        <div className="links-container show-container">
-          <ul className="links">
+
+        <div className="links-container" ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
             {links.map((link) => {
               const { id, url, text } = link;
               return (
